c053b5040e59f20482fc5704d89cb1e9ced1bbf3
ryan <ryanzhou63@gmail.com>
2025-09-15 17:14:15 -0400

    Git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/git.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/git.java
@@ -2,4 +2,14 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 public class git {
     // Connect to git
+    // git remote remove origin
+    // git remote add origin git@github.com:ryanz12/FtcRobotController.git
+    // git branch -M main
+    // git push -u origin main
+
+    // if not authenticated yet:
+    // ssh-keygen -t ed25519 -C "your_email@example.com"
+    // cat ~/.ssh/id_ed25519.pub
+    // ssh -T git@github.com
+
 }
