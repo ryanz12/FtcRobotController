e20f338363a93c5ecf2097f8a15a38a593f53b94
Ryan <ryanzhou63@gmail.com>
2025-11-12 15:44:52 -0500

    testing attention please

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/LaunchSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/LaunchSubsystem.java
@@ -7,18 +7,18 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class LaunchSubsystem extends SubsystemBase {
-    private DcMotor launchMotor;
+    private DcMotorEx launchMotor;
 
     private String launchMotorName = "launchMotor";
 
     public LaunchSubsystem(HardwareMap hwMap){
         launchMotor = hwMap.get(DcMotorEx.class, launchMotorName);
         launchMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //https://www.reddit.com/r/FTC/comments/lfl82f/whats_the_difference_between_setvelocity_and/
-        launchMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        launchMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         launchMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
     public void shoot(){
-        launchMotor.setPower(-0.75);
+        launchMotor.setVelocity(1400);
     }
 
     public void reverseShoot (){launchMotor.setPower(-0.1);}
