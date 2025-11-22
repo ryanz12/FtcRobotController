e565a63a6b63db4632660f70a8209b850aeb6e03
Themika <88339133+Themika@users.noreply.github.com>
2025-11-21 19:21:28 -0500

    Red Side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedAuto.java
@@ -14,6 +14,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 @Autonomous(name = "AutoRed",group = "Left")
+//
 public class RedAuto extends LinearOpMode {
     private DcMotor shootMotor;
     private CRServo rampServoOne;
