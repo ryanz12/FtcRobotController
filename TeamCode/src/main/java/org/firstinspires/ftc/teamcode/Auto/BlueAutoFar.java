41e9497b19aa0630ff830ccc1c1ca121d413a8ff
Ryan <ryanzhou63@gmail.com>
2025-11-26 16:23:35 -0500

    RR changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BlueAutoFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BlueAutoFar.java
@@ -2,16 +2,21 @@ package org.firstinspires.ftc.teamcode.Auto;
 
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Config
 @Autonomous
 public class BlueAutoFar extends LinearOpMode {
 
@@ -21,6 +26,7 @@ public class BlueAutoFar extends LinearOpMode {
         // Initialize
         public Launcher(HardwareMap hwMap){
             launchMotor = hwMap.get(DcMotorEx.class, "launchMotor");
+            launchMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         }
 
 
@@ -32,7 +38,7 @@ public class BlueAutoFar extends LinearOpMode {
                 @Override
                 public boolean run(@NonNull TelemetryPacket telemetryPacket) {
                     if (!initalized){
-                        launchMotor.setPower(0.8);
+                        launchMotor.setPower(1);
                         initalized = true;
                     }
 
@@ -40,22 +46,64 @@ public class BlueAutoFar extends LinearOpMode {
                     double vel = launchMotor.getVelocity();
                     telemetryPacket.put("velocity", vel);
 
-                    return vel < 10000.0;
+                    if (vel < -1450.0){
+                        launchMotor.setPower(0);
+                        return false;
+                    }
+
+                    return true;
                 }
             };
         }
+    }
 
+    private class Ramp {
+        private CRServo rampServoOne, rampServoTwo;
+        private ElapsedTime timer;
+
+        public Ramp(HardwareMap hwMap){
+            rampServoOne = hwMap.get(CRServo.class, "beltServoOne");
+            rampServoTwo = hwMap.get(CRServo.class, "beltServoTwo");
+
+            timer = new ElapsedTime();
+        }
+
+        public Action rampUp(){
+            return new Action(){
+
+                @Override
+                public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                    rampServoOne.setPower(1);
+                    rampServoTwo.setPower(-1);
+
+                    telemetryPacket.put("running true", true);
+
+                    if (timer.seconds() > 3){
+                        rampServoOne.setPower(0);
+                        rampServoTwo.setPower(0);
+                        return false;
+                    }
+
+                    return true;
+                }
+            };
+        }
     }
 
 
     @Override
     public void runOpMode(){
         Launcher launcher = new Launcher(hardwareMap);
+        Ramp ramp = new Ramp(hardwareMap);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        Actions.runBlocking(launcher.shoot());
+        Actions.runBlocking(new SequentialAction(
+                launcher.shoot(),
+                ramp.rampUp()
+
+        ));
     }
 }
