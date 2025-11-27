7476ca00bd6b99ed8a9a0f86267cf76d43d383be
Themika <88339133+Themika@users.noreply.github.com>
2025-11-27 14:43:48 -0500

    Made a Test file to test if we can use the camera to dynamically control when to stop the ramp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/DriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/DriveCommand.java
@@ -1,35 +1,28 @@
 package org.firstinspires.ftc.teamcode.command;
-
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.subsystem.DriveSubsystem;
 
 public class DriveCommand extends CommandBase {
-
     private final DriveSubsystem driveSubsystem;
     private final Gamepad gamepad;
-
     private enum DriveState {
         NORMAL,
         INVERTED
     }
-
     private DriveState currentState = DriveState.NORMAL;
     private boolean yPressedLast = false;
-
     public DriveCommand(DriveSubsystem d, Gamepad g) {
         this.driveSubsystem = d;
         this.gamepad = g;
         addRequirements(driveSubsystem);
     }
-
     @Override
     public void execute() {
         boolean yPressedNow = gamepad.y;
         if (yPressedNow && !yPressedLast) {
             toggleDriveState();
         }
-
         yPressedLast = yPressedNow;
         if (currentState == DriveState.NORMAL) {
             normalControls();
@@ -37,7 +30,6 @@ public class DriveCommand extends CommandBase {
             invertedControls();
         }
     }
-
     private void toggleDriveState() {
         if (currentState == DriveState.NORMAL) {
             currentState = DriveState.INVERTED;
@@ -45,21 +37,18 @@ public class DriveCommand extends CommandBase {
             currentState = DriveState.NORMAL;
         }
     }
-
     private void normalControls() {
         double forward = gamepad.left_stick_y;
         double strafe = -gamepad.left_stick_x;
         double turn = -gamepad.right_stick_x;
         driveSubsystem.drive(strafe, forward, turn);
     }
-
     private void invertedControls() {
         double forward = -gamepad.left_stick_y;
         double strafe = -gamepad.left_stick_x;
         double turn = - gamepad.right_stick_x;
         driveSubsystem.drive(strafe, forward, turn);
     }
-
     @Override
     public boolean isFinished() {
         // Never finish driving
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/CameraSubsystem.java
@@ -12,30 +12,21 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
 import java.util.Arrays;
 import java.util.List;
-
 public class CameraSubsystem extends SubsystemBase {
     private AprilTagProcessor aprilTag;
-
     private VisionPortal visionPortal;
-
     public CameraSubsystem(HardwareMap hMap, Telemetry telemetry){
-
         aprilTag = new AprilTagProcessor.Builder()
                 .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
                 .build();
-
         VisionPortal.Builder builder = new VisionPortal.Builder();
-
         builder.setCamera(hMap.get(WebcamName.class, "Webcam 2"));
         builder.setCameraResolution(new Size(640, 480));
         builder.addProcessor(aprilTag);
-
         visionPortal = builder.build();
     }
-
     public List<Double> getCameraData(){
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
 
@@ -46,17 +37,13 @@ public class CameraSubsystem extends SubsystemBase {
 
         return null;
     }
-
     public void startStreaming(){
         visionPortal.setProcessorEnabled(aprilTag, true);
     }
-
     public VisionPortal.CameraState getCameraState(){
         return visionPortal.getCameraState();
     }
-
     public void stopStreaming(){
         visionPortal.setProcessorEnabled(aprilTag, false);
     }
-
 }
